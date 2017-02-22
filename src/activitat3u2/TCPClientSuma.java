57754d9a65e79c3fd8ae4a52098a7b3c2c097b51
ericdote <rovx15@gmail.com>
2017-02-22 21:18:46 +0100

    Comentando.

--- src/activitat3u2/TCPClientSuma.java
+++ src/activitat3u2/TCPClientSuma.java
@@ -1,7 +1,46 @@
 package activitat3u2;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
 public class TCPClientSuma {
+    //Declarem el host i el port per defecte
+    static String HOST = "localhost";
+    static int PORT = 5487;
+    //Declarem els diferents objectes.
+    private final Socket ss;
+    private final DataOutputStream outToServer;
+    private final BufferedReader bf;
+    /**
+     * Creem el constructor que li arribara per parametre un host i un port.
+     * Dins inicialitzem els objectes i passem per teclat els dos numeros, els escribim i els enviem al Server.
+     * @param host
+     * @param port
+     * @throws IOException 
+     */
+    public TCPClientSuma(String host, int port) throws IOException {
+
+        this.ss = new Socket(host, port);
+        outToServer = new DataOutputStream(ss.getOutputStream());
+        bf = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+
+        String num1 = JOptionPane.showInputDialog(null, "Dona el primer numero: ","Entrando",3);
+        String num2 = JOptionPane.showInputDialog(null, "Dona el segon numero: ", "Entrando",3);
+        outToServer.writeInt(Integer.parseInt(num1));
+        outToServer.writeInt(Integer.parseInt(num2));
+        bf.close();
+        outToServer.close();
+        ss.close();
+
+    }
     
-    
-    
+    public static void main(String[] args) throws IOException{
+        new TCPClientSuma(HOST, PORT);
+    }
+
+
 }
--- src/activitat3u2/TCPServerSuma.java
+++ src/activitat3u2/TCPServerSuma.java
@@ -1,6 +1,36 @@
 package activitat3u2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.JOptionPane;
 
 public class TCPServerSuma {
+
+    static int PORT = 5487;
+    private final ServerSocket serverSocket;
+
+    public TCPServerSuma(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        while (true) {
+            Socket ss = serverSocket.accept();
+            DataOutputStream outToClient = new DataOutputStream(ss.getOutputStream());
+            DataInputStream bf = new DataInputStream(ss.getInputStream());
+            int num1 = bf.readInt();
+            int num2 = bf.readInt();
+            int sum = num1+num2;
+            outToClient.writeByte(sum);
+            JOptionPane.showMessageDialog(null, "El resultat es: " + sum, "Salida", 1);
+            bf.close();
+            outToClient.close();
+            ss.close();
+        }
+    }
     
+    public static void main(String[] args) throws IOException{
+        new TCPServerSuma(PORT);
+    }
+
 }
